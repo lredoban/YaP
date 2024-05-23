10a26b765c80e268e951bf1094df83a35bd00778
Lovasoa <maitrejedi@lovasoa.fr>
2024-05-23 16:38:17 +0200

    fix(pwa)l: sometimes il faut savoir renoncer. Remove PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,12 +17,7 @@ export default defineNuxtConfig({
       },
     },
   },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@vite-pwa/nuxt",
-    "@nuxt/image",
-    "@nuxtjs/i18n",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxtjs/i18n"],
   image: {
     provider: "netlify",
     domains: ["raw.githubusercontent.com"],
@@ -72,75 +67,6 @@ export default defineNuxtConfig({
         flag: "🇩🇪",
       },
     ],
-  },
-  pwa: {
-    registerType: "autoUpdate",
-    injectRegister: false,
-    manifest: {
-      name: "Yet Another Pokédex",
-      short_name: "YaP",
-      description: "A Pokedex application built with Nuxt.js",
-      theme_color: "#00DC82",
-      background_color: "#ffffff",
-      display: "standalone",
-      scope: "/",
-      start_url: "/",
-      icons: [
-        {
-          src: "pwa-64x64.png",
-          sizes: "64x64",
-          type: "image/png",
-        },
-        {
-          src: "pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any",
-        },
-        {
-          src: "maskable-icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable",
-        },
-      ],
-      screenshots: [
-        {
-          src: "Screenshot-desktop.png",
-          sizes: "2406x1672",
-          type: "image/png",
-          form_factor: "wide",
-          label: "Homescreen Desktop",
-        },
-        {
-          src: "Screenshot-mobile.png",
-          sizes: "698x1506",
-          type: "image/webp",
-          label: "Homescreen Mobile",
-        },
-      ],
-    },
-    workbox: {
-      globPatterns: [],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
-    devOptions: {
-      enabled: false,
-      suppressWarnings: true,
-      navigateFallback: "/",
-      navigateFallbackAllowlist: [/^\/$/],
-      type: "module",
-    },
-    registerWebManifestInRouteRules: true,
-    client: {
-      installPrompt: true,
-    },
   },
   $development: {
     devtools: { enabled: true },
